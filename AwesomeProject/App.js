51923c866f29c804a4ee334d35c544f70252dc17
naoki <n.globe.us@gmail.com>
2019-12-21 23:03:21 +0900

    fix some

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,19 +1,33 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { Text, TextInput, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+export default class PizzaTranslator extends Component {
+  //コストラクタ。初期化された時に呼び出される。
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
   }
-});
+
+  render() {
+    return (
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{ top: 300, height: 40 }}
+          placeholder="Type here to translate!"
+          //入力された文字をtextに保存。
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text
+            //splitで" "ごとに分割して配列を作成。
+            .split(" ")
+            //各値がtrueであればピザ変換した配列を作成。
+            .map(word => word && "🍕")
+            //半角スペースで結合し、文字列に変換。
+            .join(" ")}
+        </Text>
+      </View>
+    );
+  }
+}
