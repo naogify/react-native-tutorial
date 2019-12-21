f15f90a4b6d9ed9ce72ab7b2d0f2f2bcc93e1ee9
naoki <n.globe.us@gmail.com>
2019-12-21 22:23:26 +0900

    add some

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,19 +1,50 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
+class Blink extends Component {
+  //コンポートがマウント（treeに挿入）された時呼び出される関数。
+  componentDidMount() {
+    // Toggle the state every second
+    setInterval(
+      () =>
+        //previousStateを引数としてとり、isShowingTextに previousState.isShowingText と反対の値を保存する。
+        this.setState(
+          previousState => (
+            console.log(previousState),
+            {
+              isShowingText: !previousState.isShowingText
+            }
+          )
+        ),
+      //それを1秒ごとに実行。
+      1000
+    );
+  }
+
+  //state object
+  //コンポートが初期化（最初に呼び出された時）、isShowingTextをtrueに設定。
+  state = { isShowingText: true };
+
+  render() {
+    //this.state.isShowingTextの値が、true でなければ null を返す。
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    //true だったらpropsで渡されたテキストを表示。
+    return <Text>{this.props.text}</Text>;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text="I love to blink" />
+        <Blink text="Yes blinking is so great" />
+        <Blink text="Why did they ever take this out of HTML" />
+        <Blink text="Look at me look at me look at me" />
+      </View>
+    );
   }
-});
+}
