9062069d4808928e4c60e32ef750a40f3f16bc09
naoki <n.globe.us@gmail.com>
2019-12-21 23:10:08 +0900

    add some

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,19 +1,45 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { Button, StyleSheet, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    alert("You tapped the button!");
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          {/* onPressでclickイベント発火時に呼び出す関数を指定 */}
+          <Button onPress={this._onPressButton} title="Press Me" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button onPress={this._onPressButton} title="This looks great!" />
+          <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center"
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: "row",
+    justifyContent: "space-between"
   }
 });
