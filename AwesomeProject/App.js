6aac32577ccad728588365aded25f460e71fbd23
naoki <n.globe.us@gmail.com>
2019-12-21 22:48:52 +0900

    add some

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+export default class FlexDirectionBasics extends Component {
+  render() {
+    return (
+      // Try setting `flexDirection` to `column`.
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View
+          style={{ width: 50, height: 50, backgroundColor: "powderblue" }}
+        />
+        <View style={{ width: 50, height: 50, backgroundColor: "skyblue" }} />
+        <View style={{ width: 50, height: 50, backgroundColor: "steelblue" }} />
+      </View>
+    );
   }
-});
+}
