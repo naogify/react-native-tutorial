941eab964eb38343052abb5cabf8ea4cecd3137b
naoki <n.globe.us@gmail.com>
2019-12-21 22:43:43 +0900

    fix some

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+export default class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, backgroundColor: "powderblue" }} />
+        <View style={{ flex: 2, backgroundColor: "skyblue" }} />
+        <View style={{ flex: 3, backgroundColor: "steelblue" }} />
+      </View>
+    );
   }
-});
+}
